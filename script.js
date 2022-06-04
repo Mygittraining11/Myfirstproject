5c584af2d90eadca243a771848372ef468f7ecce
Mygittraining11 <kristtenmitchell@gmail.com>
2022-06-03 23:54:26 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+console.log(document.querySelector('textarea').value);
+
+document.querySelector('button').addEventListener('click', function () {
+  prompt('We have taken and are processing your details..');
+});
